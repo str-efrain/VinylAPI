a471ca011894d692b190516cd1f97484267df544
Efrain Mouton <efrain.mouton@student.vives.be>
2026-05-15 17:38:50 +0200

    Admin middleware toegevoegd die voor authorizatie zorgt.

---
+++ src/middleware/admin.middleware.js
@@ -0,0 +1,9 @@
+function adminMiddleware(req, res, next) {
+  if (req.user.role !== 'admin') {
+    return res.status(403).send('Access denied.');
+  }
+
+  next();
+}
+
+module.exports = adminMiddleware;
